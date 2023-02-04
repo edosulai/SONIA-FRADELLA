eddd9a25972a78588aca36594ed7359b71d5b851
Edo Sulai <ngufeel@gmail.com>
2023-02-04 23:22:48 +0700

    Http to Https

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import laravel from 'laravel-vite-plugin'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    server: {
+        https: true,
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost'
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
@@ -10,4 +17,4 @@ export default defineConfig({
         }),
         react(),
     ],
-});
+})
