cbbe0605e43580cec7a7ce5e99f5bbb25ff96851
Edo Sulai <ngufeel@gmail.com>
2023-02-05 12:23:23 +0700

    Menghilangkan Route /

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import laravel from 'laravel-vite-plugin'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    server: {
-        https: true,
-        host: '0.0.0.0',
-        hmr: {
-            host: 'localhost'
-        },
-    },
+    // server: {
+    //     https: true,
+    //     host: '0.0.0.0',
+    //     hmr: {
+    //         host: 'localhost'
+    //     },
+    // },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
