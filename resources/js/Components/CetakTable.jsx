f549824db332e7ad42a7b7780bb9e8997e821461
Edo Sulaiman <edo@steradian.co.id>
2024-01-16 23:56:38 +0700

    Improvement for Letters

--- resources/js/Components/CetakTable.jsx
+++ resources/js/Components/CetakTable.jsx
@@ -5,20 +5,30 @@ export default function CetakTable({ ctx, tableRef }) {
     return (
         <button
             onClick={() => {
-                const doc = new jsPDF(ctx.orientation ?? "potrait");
+                const doc = new jsPDF(ctx.orientation ?? "portrait");
 
                 doc.autoTable({
                     html: tableRef.current,
-                    // addPageContent: function (data) {
-                    //     doc.text("KOP SURAT", 40, 30);
-                    //     doc.text("Ini adalah contoh kop surat", 40, 50);
-                    // },
-                    margin: { top: 27 },
+                    addPageContent: function (data) {
+                        // console.log(doc.getFontList());
+
+                        doc.setFontSize(18);
+                        doc.setFont("Times", "Bold");
+                        doc.text("Puskesmas Kemantan", doc.internal.pageSize.width / 2, 15, { align: "center" });
+                        doc.setFontSize(8);
+                        doc.setFont("Times", "Italic");
+                        doc.text("Jl. Raya Kemantan, Kemantan Darat, Kec. Air Hangat Timur, Kab. Kerinci, Jambi, 37167", doc.internal.pageSize.width / 2, 20, { align: "center" });
+                        doc.setFont("Times", "Italic");
+                        doc.text("Telp: (0748)353081", doc.internal.pageSize.width / 2, 25, { align: "center" });
+
+                        doc.setFontSize(12);
+                        doc.setFont("Times", "Roman");
+                        doc.text("..........., ........................", doc.internal.pageSize.width / 1.25, doc.internal.pageSize.height - 70, { align: "center" });
+                        doc.text("Kepala Puskesmas Kemantan", doc.internal.pageSize.width / 1.25, doc.internal.pageSize.height - 60, { align: "center" });
+                        doc.text("(                                            )", doc.internal.pageSize.width / 1.25, doc.internal.pageSize.height - 20, { align: "center" });
+                    },
+                    margin: { top: 30, bottom: 80 },
                     didDrawPage: function (data) {
-                        // doc.setFontSize(18);
-                        // doc.setTextColor(40);
-                        // doc.setFontStyle("normal");
-                        //doc.addImage(headerImgData, 'JPEG', data.settings.margin.left, 20, 50, 50);
                         doc.text(ctx.nama, data.settings.margin.left, 22);
                     },
                 });
